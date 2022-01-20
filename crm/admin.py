c65b0f84b3666444962f82ba990a760959021c8a
AdilakshmiBonala <bonalaadilakshmi938@gmail.com>
2022-01-20 18:24:59 +0530

    update admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from crm.models import *
 
+
+class EnquiryDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'phone_number',
+                    'country_code', 'course_name', 'is_public_enquiry',
+                    'submitted_at']
+
+
+class UserEnquiryDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'enquiry_details']
+
+
 admin.site.register(UserAccount)
-admin.site.register(EnquiryDetails)
-admin.site.register(UserEnquiryDetails)
+admin.site.register(EnquiryDetails, EnquiryDetailsAdmin)
+admin.site.register(UserEnquiryDetails, UserEnquiryDetailsAdmin)
