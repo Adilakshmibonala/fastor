1909f31a0fbd89dc7e8f035db9434550bfec1d61
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-03-10 21:29:22 +0530

    Update settings

--- fastor/settings.py
+++ fastor/settings.py
@@ -75,20 +75,10 @@ WSGI_APPLICATION = 'fastor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRE_SQL_DB_NAME'],
-        'USER': os.environ['POSTGRE_SQL_USER_NAME'],
-        'PASSWORD': os.environ['POSTGRE_SQL_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
