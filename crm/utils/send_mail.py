6bcefa1eb813bd257ac7ce537288c7ab03d844b6
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-05-01 12:05:40 +0530

    Minor changes

--- crm/utils/send_mail.py
+++ crm/utils/send_mail.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 
 def send_email_notification(subject: str, message: str, to_email: str):
-    response = send_mail(
+    send_mail(
         subject=subject, message=message, from_email=settings.EMAIL_HOST_USER,
         recipient_list=[to_email], fail_silently=False)
-    print("response", response)
