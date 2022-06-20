cddf829339a5a127027de30afd4398c543a8c63c
AdilakshmiBonala <adilakshmi.bonala@techgrit.com>
2022-06-20 21:05:23 +0530

    Add app files

--- fastor/settings.py
+++ fastor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +137,17 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+sentry_sdk.init(
+    dsn="https://9f08aae8b03d4cd48112f48e1b7b7279@o1234385.ingest.sentry.io/6383660",
+    integrations=[DjangoIntegration()],
+    environment="dev",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
